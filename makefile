9bc38459207c201391ac509d6dec944448055a9b
sofia-am <sofiamallo07@hotmail.com>
2022-05-15 17:16:25 -0300

    probando libreria jansson

--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 CC = gcc 
-CFLAGS = -g -Wall -pedantic -Wextra -std=gnu11 -Wconversion -Werror 
+CFLAGS = -g -Wall -pedantic -Wextra -std=gnu11 -Wconversion #-Werror 
 OBJS = obj#nombre de la carpeta donde se guardan los .o
 SRC = src
 BIN = bin
@@ -14,10 +14,10 @@ mkdir:
 	mkdir -p $(SRC) $(BIN) $(LIB) $(OBJS) $(INC)
 
 main.o: $(SRC)/test.c
-	$(CC) $(CFLAGS) -c -lulfius $< -o $(OBJS)/$@
+	$(CC) $(CFLAGS) -c -lulfius -ljansson $< -o $(OBJS)/$@
 
 main: main.o
-	$(CC) $(OBJS)/$< -lulfius -o $@
+	$(CC) $(OBJS)/$< -lulfius -ljansson -o $@
 
 clean:
 	rm -rf main main.o
--- src/test.c
+++ src/test.c
@@ -6,28 +6,56 @@
  */
 #include <stdio.h>
 #include <ulfius.h>
+#include <jansson.h>
 
 #define PORT 8537
 
 int acumulador;
+/**
+ * Decode a u_map into a string
+ */
+char * print_map(const struct _u_map * map) {
+  char * line, * to_return = NULL;
+  const char **keys, * value;
+  int len, i;
+  if (map != NULL) {
+    keys = u_map_enum_keys(map);
+    for (i=0; keys[i] != NULL; i++) {
+      value = u_map_get(map, keys[i]);
+      len = snprintf(NULL, 0, "key is %s, value is %s", keys[i], value);
+      line = malloc((len+1)*sizeof(char));
+      snprintf(line, (len+1), "key is %s, value is %s", keys[i], value);
+      if (to_return != NULL) {
+        len = strlen(to_return) + strlen(line) + 1;
+        to_return = realloc(to_return, (len+1)*sizeof(char));
+        if (strlen(to_return) > 0) {
+          strcat(to_return, "\n");
+        }
+      } else {
+        to_return = malloc((strlen(line) + 1)*sizeof(char));
+        to_return[0] = 0;
+      }
+      strcat(to_return, line);
+      free(line);
+    }
+    return to_return;
+  } else {
+    return NULL;
+  }
+}
 
 /**
  * Callback function for the web application on /prueba url call
  */
 int callback_set(__attribute__((unused))const struct _u_request * request, struct _u_response * response, __attribute__((unused))void * user_data) {
-  acumulador++;
+  //acumulador++;
+  char *url_params = print_map(request->map_url), *headers = print_map(request->map_header), *post_params = print_map(request->map_post_body);
+  char *response_body;
+  printf("La información que obtuve es: \nurl_params: %s, \nheaders: \n%s, \npost_params: \n%s", url_params, headers, post_params);
+  return U_CALLBACK_CONTINUE;
   ulfius_set_string_body_response(response, 200, "Ok\n");
   return U_CALLBACK_CONTINUE;
 }
-
-/* int callback_post_test (const struct _u_request * request, struct _u_response * response, void * user_data) {
-  char * post_params = print_map(request->map_post_body);
-  char * response_body = msprintf("Hello World!\n%s", post_params);
-  ulfius_set_string_body_response(response, 200, response_body);
-  o_free(response_body);
-  o_free(post_params);
-  return U_CALLBACK_CONTINUE;
-} */
 
 int callback_get(__attribute__((unused))const struct _u_request * request, struct _u_response * response, __attribute__((unused))void * user_data){
   char *string = malloc(sizeof(char)*30);
@@ -36,6 +64,22 @@ int callback_get(__attribute__((unused))const struct _u_request * request, struc
   return U_CALLBACK_CONTINUE;
 }
 
+int callback_test (const struct _u_request * request, struct _u_response * response, void * user_data) {
+  char *url_params = print_map(request->map_url), *headers = print_map(request->map_header); //*post_params = print_map(request->map_post_body);
+  char *response_body;
+  printf("La información que obtuve es: \nurl_params: %s, \nheaders: \n%s, \n", url_params, headers);
+  json_t *params = json_object();
+  json_error_t * error;
+  params = ulfius_get_json_body_request(request, error);
+  const char *key;
+  json_t *value;
+  size_t len;
+
+  json_object_foreach(params, key, value){
+      printf("Par clave valor: |%s||%s|\n", key, json_string_value(value));
+  }
+  return U_CALLBACK_CONTINUE;
+}
 /**
  * main function
  */
@@ -52,6 +96,7 @@ int main(void) {
   // Endpoint list declaration
   ulfius_add_endpoint_by_val(&instance, "GET", "/imprimir", NULL, 0, &callback_get, NULL);
   ulfius_add_endpoint_by_val(&instance, "POST", "/increment", NULL, 0, &callback_set, NULL);
+  ulfius_add_endpoint_by_val(&instance, "POST", "/add", NULL, 0, &callback_test, NULL);
 
   // Start the framework
   if (ulfius_start_framework(&instance) == U_OK) {
